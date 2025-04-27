981fb2aa49107f5dee12a8f3e06b494b32cf2efc
khanhviet5903 <vietkhanh5903@gmail.com>
2025-04-27 02:38:15 +0200

    second commit

--- api_project/src/App.jsx
+++ api_project/src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 function App() {
   const [count, setCount] = useState(0); 
-
+  const [searchTerm, setSearchTerm] = useState(''); 
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(null);
 
@@ -21,10 +21,30 @@ function App() {
     }
     fetchCountries();
   }, []);
+  if (isLoading) {
+    return <div>Loading ...</div>
+  } if (error) {
+    return <div> Error: {error} </div>
+  }
 
   return (
-    <>
-    </>
+    <div className="container"> 
+    <h1>Länder und Hauptstädte</h1>
+    <ul>
+      {countries.map((country)=> (
+        <li key= {country.cca3}>
+           <h2>{country.name.common} </h2>
+           <p>{country.capital?.[0] || "N/A"}</p>
+           {country.flags?.png && 
+           <img src= {country.flags.png} 
+            className= "country-flag"
+           alt= {`Flag of ${country.name.common}`} />}
+        </li>
+      ))}
+    </ul>
+    </div> 
+    
+    
   );
 }
 
